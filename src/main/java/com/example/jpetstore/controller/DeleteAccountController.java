a54538012c9743e1b17bcbe4b2f40e8595763f4a
hello <hello@DESKTOP-20TDTIA>
2022-06-02 16:42:06 +0900

    update: DeleteAccountController 수정

--- src/main/java/com/example/jpetstore/controller/DeleteAccountController.java
+++ src/main/java/com/example/jpetstore/controller/DeleteAccountController.java
@@ -1,10 +1,29 @@
 package com.example.jpetstore.controller;
+import javax.servlet.http.HttpSession;
 
-public class DeleteAccountController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+import com.example.jpetstore.domain.Cart;
+import com.example.jpetstore.service.SosoMarketFacade;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+@Controller
+@SessionAttributes("sessionCart")
+public class DeleteAccountController { 
 
+	@RequestMapping("/user/deleteUser.do")
+	public ModelAndView handleRequest(
+			@RequestParam("accountId") String accountId,
+			@ModelAttribute("sosoMarket") SosoMarketFacade sosoMarket,
+			HttpSession session
+		) throws Exception {
+		
+		sosoMarket.deleteAccount(accountId);
+		
+		
+		return "index";
 	}
-
 }
