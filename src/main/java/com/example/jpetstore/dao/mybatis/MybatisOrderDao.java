b2edff79ae82229c72ba5b1b4ab2d5a123d9e360
jeeyun2 <irisjeeyun@naver.com>
2022-06-11 20:22:03 +0900

    Bidding&Order DAO input

--- src/main/java/com/example/jpetstore/dao/mybatis/MybatisOrderDao.java
+++ src/main/java/com/example/jpetstore/dao/mybatis/MybatisOrderDao.java
@@ -3,13 +3,9 @@ package com.example.jpetstore.dao.mybatis;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.example.jpetstore.dao.OrderDao;
-import com.example.jpetstore.dao.SequenceDao;
-import com.example.jpetstore.dao.mybatis.mapper.LineItemMapper;
 import com.example.jpetstore.dao.mybatis.mapper.OrderMapper;
-import com.example.jpetstore.domain.LineItem;
 import com.example.jpetstore.domain.Order;
 
 import org.springframework.dao.DataAccessException;
@@ -20,34 +16,28 @@ public class MybatisOrderDao implements OrderDao {
 	
 	@Autowired
 	protected OrderMapper orderMapper;
-	@Autowired
-	protected LineItemMapper lineItemMapper;
-	@Autowired
-	private SequenceDao sequenceDao;
 
-	public List<Order> getOrdersByUsername(String username) 
-			throws DataAccessException {
-	    return orderMapper.getOrdersByUsername(username);
+	public Order getOrderBySeller(int orderId) throws DataAccessException {
+		return orderMapper.getOrderBySeller(orderId);
 	}
 	
-	@Transactional
-	public Order getOrder(int orderId) throws DataAccessException {
-		Order order = orderMapper.getOrder(orderId);
-		if (order != null) {
-			order.setLineItems(lineItemMapper.getLineItemsByOrderId(orderId));
-		}
-	    return order;
+	public Order getOrderByBuyer(int orderId) throws DataAccessException {
+		return orderMapper.getOrderByBuyer(orderId);
+	}
+
+	public void insertOrder(Order order) throws DataAccessException {
+		orderMapper.insertOrder(order);
+	}
+
+	public void updateOrderStatus(Order order) throws DataAccessException {
+		orderMapper.updateOrderStatus(order);
+	}
+
+	public List<Order> getOrderListBySeller(String accountId) throws DataAccessException {
+		return orderMapper.getOrderListBySeller(accountId);
 	}
 	
-	@Transactional
-	public void insertOrder(Order order) throws DataAccessException {  
-    	order.setOrderId(sequenceDao.getNextId("ordernum"));
-    	orderMapper.insertOrder(order);
-    	orderMapper.insertOrderStatus(order);
-    	for (int i = 0; i < order.getLineItems().size(); i++) {
-    		LineItem lineItem = (LineItem) order.getLineItems().get(i);
-    		lineItem.setOrderId(order.getOrderId());
-    		lineItemMapper.insertLineItem(lineItem);
-    	}
+	public List<Order> getOrderListByBuyer(String accountId) throws DataAccessException {
+		return orderMapper.getOrderListByBuyer(accountId);
 	}
 }
