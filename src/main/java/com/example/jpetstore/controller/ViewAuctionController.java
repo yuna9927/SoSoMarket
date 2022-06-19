6b99e8567e12a944a55b9aa863956fa449fa3feb
hello <hello@DESKTOP-20TDTIA>
2022-06-19 20:47:43 +0900

    xxcc

--- src/main/java/com/example/jpetstore/controller/ViewAuctionController.java
+++ src/main/java/com/example/jpetstore/controller/ViewAuctionController.java
@@ -2,15 +2,11 @@ package com.example.jpetstore.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.util.WebUtils;
 import com.example.jpetstore.domain.Auction;
 import com.example.jpetstore.domain.Bidding;
 import com.example.jpetstore.service.SosoMarketFacade;
