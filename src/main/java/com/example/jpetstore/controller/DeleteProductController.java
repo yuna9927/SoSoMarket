032d720c6cd894ff190a830cb58927bfdb246021
doubletry23 <doubletry23@gmail.com>
2022-06-19 18:07:53 +0900

    create: 경매 상품 삭제

--- src/main/java/com/example/jpetstore/controller/DeleteProductController.java
+++ src/main/java/com/example/jpetstore/controller/DeleteProductController.java
@@ -29,45 +29,50 @@ public class DeleteProductController {
 	private SosoMarketFacade sosomarket;
 
 	@Autowired
-	public void setPetStore(SosoMarketFacade petSsosomarkettore) {
+	public void setPetStore(SosoMarketFacade sosomarket) {
 		this.sosomarket = sosomarket;
 	}
 
 	@RequestMapping("/shop/deleteProduct.do")
 	public ModelAndView deleteProduct(HttpSession session,
-			@RequestParam("productId") String productId,
+			@RequestParam("productId") int productId,
 			ModelMap model) throws Exception {
 		//로그인 사용자 != 상품 게시 사용자일 경우 error
 		UserSession userSession = (UserSession) session.getAttribute("userSession");
-		Product product = sosomarket.getProduct(Integer.parseInt(productId));
+		Product product = sosomarket.getProduct(productId);
 		System.out.println("현재로그인 id : " + userSession.getAccount().getAccountId() + ", " + product.getSellerId());
 		if (!userSession.getAccount().getAccountId().equals(product.getSellerId()))
 			return new ModelAndView("Error", "message", 
 					"본인이 등록한 상품만 삭제할 수 있습니다.");
 		//상품 삭제가 가능할 경우 삭제
 		else {
-			this.sosomarket.deleteProduct(Integer.parseInt(productId));
+			this.sosomarket.deleteProduct(productId);
 		
-			return new ModelAndView("SearchProductList");
+			return new ModelAndView("index");
 		}
 	}
 	
-	@RequestMapping("/shop/deleteAuction.do")
+	@RequestMapping("//shop/deleteAuctionProduct.do")
 	public ModelAndView deleteAuctionProduct(HttpSession session,
-			@RequestParam("auctionId") String auctionId,
+			@RequestParam("auctionId") int auctionId,
 			ModelMap model) throws Exception {
 		//로그인 사용자 != 상품 게시 사용자일 경우 error
 		UserSession userSession = (UserSession) session.getAttribute("userSession");
-		Auction auction = sosomarket.getAuction(Integer.parseInt(auctionId));
-		System.out.println("현재로그인 id : " + userSession.getAccount().getAccountId() + ", " + auction.getProduct().getSellerId());
-		if (!userSession.getAccount().getAccountId().equals(auction.getProduct().getSellerId()))
+		Auction auction = sosomarket.getAuction(auctionId);
+		String loginId = userSession.getAccount().getAccountId();
+		System.out.println("현재로그인 id : " + loginId + ", " + auction.getProduct().getSellerId());
+		if (!loginId.equals(auction.getProduct().getSellerId()))
 			return new ModelAndView("Error", "message", 
 					"본인이 등록한 상품만 삭제할 수 있습니다.");
+		if (auction.getProduct().getProductStatus().equals("done"))
+			return new ModelAndView("Error", "message", 
+					"주문된 상품은 삭제할 수 없습니다.");
 		//상품 삭제가 가능할 경우 삭제
 		else {
-			this.sosomarket.deleteProduct(Integer.parseInt(auctionId));
+			this.sosomarket.updateWithdraw(loginId); //철회 횟수 증가
+			this.sosomarket.deleteAuction(auctionId);
 		
-			return new ModelAndView("SearchAuctionProductList");
+			return new ModelAndView("index");
 		}
 	}
 	
