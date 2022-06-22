a63b69e560c4435f5f5cc423a22fb51727e37032
doubletry23 <doubletry23@gmail.com>
2022-06-22 17:11:53 +0900

    update: product & auction 분리 검색

--- src/main/java/com/example/jpetstore/controller/SearchProductsController.java
+++ src/main/java/com/example/jpetstore/controller/SearchProductsController.java
@@ -9,13 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.jpetstore.domain.Auction;
 import com.example.jpetstore.domain.Product;
 import com.example.jpetstore.service.SosoMarketFacade;
 
 @Controller
-public class SearchProductsController { 
+public class SearchProductsController {
 
 	private SosoMarketFacade sosoMarket;
+	private String successViewName;
 
 	@Autowired
 	public void setSoSoMarket(SosoMarketFacade sosoMarket) {
@@ -24,33 +26,48 @@ public class SearchProductsController {
 
 	@RequestMapping("/shop/searchProducts.do")
 	public ModelAndView handleRequest(HttpServletRequest request,
-			@RequestParam(value="keyword", required=false) String keyword,
-			@RequestParam(value="page", required=false) String page
-			) throws Exception {
+			@RequestParam(value = "keyword", required = false) String keyword,
+			@RequestParam(value = "page", required = false) String page,
+			@RequestParam(value = "type", required = false) String type) throws Exception {
 		if (keyword != null) {
 			if (!StringUtils.hasLength(keyword)) {
-				return new ModelAndView("Error", "message", "Please enter a keyword to search for, then press the search button.");
+				return new ModelAndView("Error", "message",
+						"Please enter a keyword to search for, then press the search button.");
 			}
-			PagedListHolder<Product> productList = new PagedListHolder<Product>(this.sosoMarket.searchProductList(keyword.toLowerCase()));
-			productList.setPageSize(4);
-			request.getSession().setAttribute("SearchProductsController_productList", productList);
-			return new ModelAndView("SearchProductList", "productList", productList);
-		}
-		else {
+
+			System.out.println("type: " + type);
+			
+			//search product
+			if (type.equals("product")) {
+				PagedListHolder<Product> productList = new PagedListHolder<Product>(
+						this.sosoMarket.searchProductList(keyword.toLowerCase()));
+				productList.setPageSize(4);
+				request.getSession().setAttribute("SearchProductsController_productList", productList);
+				return new ModelAndView("SearchProductList", "productList", productList);
+			}
+			//search auction
+			else {
+				PagedListHolder<Auction> auctionList = new PagedListHolder<Auction>(
+						this.sosoMarket.searchAuctionList(keyword.toLowerCase()));
+				auctionList.setPageSize(4);
+				request.getSession().setAttribute("SearchProductsController_auctionList", auctionList);
+				return new ModelAndView("SearchAuctionList", "auctionList", auctionList);
+			}
+			
+		} else {
 			@SuppressWarnings("unchecked")
-			PagedListHolder<Product> productList = (PagedListHolder<Product>)request.getSession().getAttribute("SearchProductsController_productList");
+			PagedListHolder<Product> productList = (PagedListHolder<Product>) request.getSession()
+					.getAttribute("SearchProductsController_productList");
 			if (productList == null) {
 				return new ModelAndView("Error", "message", "Your session has timed out. Please start over again.");
 			}
 			if ("next".equals(page)) {
 				productList.nextPage();
-			}
-			else if ("previous".equals(page)) {
+			} else if ("previous".equals(page)) {
 				productList.previousPage();
 			}
 			return new ModelAndView("SearchProducts", "productList", productList);
 		}
 	}
-	
 
 }
