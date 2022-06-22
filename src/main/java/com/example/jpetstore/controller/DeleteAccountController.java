c4678e2f31875c6a7ff76f16dd1a21aada89f79f
doubletry23 <doubletry23@gmail.com>
2022-06-22 21:26:12 +0900

    update: delete account

--- src/main/java/com/example/jpetstore/controller/DeleteAccountController.java
+++ src/main/java/com/example/jpetstore/controller/DeleteAccountController.java
@@ -3,29 +3,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.jpetstore.service.SosoMarketFacade;
 
 @Controller
 @SessionAttributes("userSession")
+@RequestMapping("/user/deleteUser.do")
 public class DeleteAccountController { 
 
-	
+	@Autowired
 	private SosoMarketFacade sosoMarket;
 	
 	@Autowired
 	public void setSosomarket(SosoMarketFacade sosoMarket) {
 		this.sosoMarket = sosoMarket;
 	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String showForm() {
+	      return "DeleteAccountForm";
+	}
 	
-	@RequestMapping("/user/deleteUser.do")
+	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView handleRequest(
 			@ModelAttribute("userSession") UserSession userSession
 		) throws Exception {
 		
 		String accountId = userSession.getAccount().getAccountId();
-//		Account account = sosoMarket.getAccount(accountId);
 		sosoMarket.deleteAccount(accountId);
 		
 		return new ModelAndView("index");
