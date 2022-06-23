60b098a7bfb8a9e7db979681146603b5dc9e8eb8
hello <hello@DESKTOP-20TDTIA>
2022-06-23 18:36:58 +0900

    이것저것 고침

--- src/main/java/com/example/jpetstore/controller/DeleteProductController.java
+++ src/main/java/com/example/jpetstore/controller/DeleteProductController.java
@@ -25,33 +25,36 @@ public class DeleteProductController {
 	}
 
 	@RequestMapping("/shop/deleteProduct.do")
-	public ModelAndView deleteProduct(HttpSession session,
-			@RequestParam("productId") int productId,
-			ModelMap model) throws Exception {
-		UserSession userSession = (UserSession) session.getAttribute("userSession");
-		Product product = sosomarket.getProduct(productId);
-		
-		if (!userSession.getAccount().getAccountId().equals(product.getSellerId()))
-			return new ModelAndView("Error", "message", 
-					"������ ����� ��ǰ�� ������ �� �ֽ��ϴ�.");
-		if (product.getProductStatus().equals("done"))
-			return new ModelAndView("Error", "message", 
-					"�ֹ��� ��ǰ�� ������ �� �����ϴ�.");
-		else {
-			this.sosomarket.deleteProduct(productId);
-		
-			return new ModelAndView("index");
-		}
-	}
+	   public ModelAndView deleteProduct(HttpSession session,
+	         @RequestParam("productId") int productId,
+	         ModelMap model) throws Exception {
+	      UserSession userSession = (UserSession) session.getAttribute("userSession");
+	      Product product = sosomarket.getProduct(productId);
+	      
+	      if (!userSession.getAccount().getAccountId().equals(product.getSellerId()))
+	         return new ModelAndView("Error", "message", 
+	               "占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙占  占쏙옙품占쏙옙 占쏙옙占쏙옙占쏙옙 占쏙옙 占쌍쏙옙占싹댐옙.");
+	      if (product.getProductStatus().equals("done"))
+	         return new ModelAndView("Error", "message", 
+	               "占쌍뱄옙占쏙옙 占쏙옙품占쏙옙 占쏙옙占쏙옙占쏙옙 占쏙옙 占쏙옙占쏙옙占싹댐옙.");
+	      else {
+	         this.sosomarket.deleteProduct(productId);
+	      
+	         return new ModelAndView("index");
+	      }
+	   }
+
 	
 	@RequestMapping("/shop/deleteAuctionProduct.do")
 	public ModelAndView deleteAuctionProduct(HttpSession session,
-			@RequestParam("auctionId") int auctionId,
+			@RequestParam("productId") int productId,
 			ModelMap model) throws Exception {
+		
+		System.out.println("넘어온 product is: " + productId);
 		UserSession userSession = (UserSession) session.getAttribute("userSession");
-		Auction auction = sosomarket.getAuction(auctionId);
+		Auction auction = sosomarket.getAuction(productId);
 		String loginId = userSession.getAccount().getAccountId();
-		System.out.println("����α��� id : " + loginId + ", " + auction.getProduct().getSellerId());
+		
 		if (!loginId.equals(auction.getProduct().getSellerId()))
 			return new ModelAndView("Error", "message", 
 					"������ ����� ��ǰ�� ������ �� �ֽ��ϴ�.");
@@ -60,7 +63,8 @@ public class DeleteProductController {
 					"�ֹ��� ��ǰ�� ������ �� �����ϴ�.");
 		else {
 			this.sosomarket.updateWithdraw(loginId);
-			this.sosomarket.deleteAuction(auctionId);
+			this.sosomarket.deleteProduct(productId);
+			this.sosomarket.deleteAuction(productId);
 		
 			return new ModelAndView("index");
 		}
