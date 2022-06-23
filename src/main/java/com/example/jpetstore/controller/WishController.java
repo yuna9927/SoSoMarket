103a8c53dcc5493e4750c635215d9a9cdc5fee08
doubletry23 <doubletry23@gmail.com>
2022-06-23 17:05:28 +0900

    update: wish 담긴 여부 판별 변수 보내줌

--- src/main/java/com/example/jpetstore/controller/WishController.java
+++ src/main/java/com/example/jpetstore/controller/WishController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,7 +34,8 @@ public class WishController {
 	public String handleRequest(
 			@RequestParam("productId") String productId,
 			@ModelAttribute("wish") Wish wish,
-			HttpServletRequest request
+			HttpServletRequest request,
+			ModelMap model
 			) throws Exception {
 //		if (cart.containsItemId(workingItemId)) {
 //			cart.incrementQuantityByItemId(workingItemId);
@@ -56,9 +58,11 @@ public class WishController {
 			wish2 = wish;
 			wish2.setAccountId(userSession.getAccount().getAccountId());
 			wish2.setProductId(Integer.parseInt(productId));
+			model.put("isIn", "true");
 			sosomarket.insertWish(wish2);
 		} else {
 			sosomarket.deleteWish(Integer.parseInt(productId));
+			model.put("isIn", "false");
 		}
 		
 //		return new ModelAndView("Wish", "wish", wish2);
